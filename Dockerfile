8cc9a5c5bc6190a4d8b4399854045b859bf61ee6
Pieter Edelman <edelman@nictiz.nl>
2020-12-14 22:21:52 +0100

    First stab at Docker based action

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM ubuntu:20.10
+RUN apt-get update && apt-get -y upgrade
+RUN apt-get -y install wget
+RUN apt-get -y install openjdk-11-jre-headless
+RUN apt-get -y install python3
+
+RUN mkdir /tools
+RUN wget -nv https://github.com/hapifhir/org.hl7.fhir.core/releases/latest/download/validator_cli.jar -O tools/validator.jar
+RUN java -jar tools/validator.jar -version 4.0 | cat
+
+COPY analyze_results.py /tools/analyze_results.py
+
+RUN mkdir /output
+COPY entrypoint.sh /entrypoint.sh
+CMD ["./entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+result=/output/result.xml
+
+function setParameter {
+  param_str=""
+  if [ $2 != "none" ]; then
+    param_str="-$1 $2"
+  fi
+  echo $param_str
+}
+
+allow_level=2
+
+optstring=":i:r:t:p:l:a:"
+while getopts ${optstring} arg; do
+  case ${arg} in
+    i)
+      ig=$(setParameter "ig" ${OPTARG})
+      ;;
+    r)
+      recurse=$(setParameter "recurse" ${OPTARG})
+      ;;
+    t)
+      tx=$(setParameter "tx" ${OPTARG})
+      ;;
+    p)
+      profile=$(setParameter "profile" ${OPTARG})
+      ;;
+    l)
+      language=$(setParameter "language" ${OPTARG})
+      ;;
+    a)
+      allow_level=${OPTARG}
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+source=$@
+
+echo "::group::Run the validator"
+java -jar /tools/validator.jar -version 4.0 $ig $recurse $tx $profile $language -output $result $source
+echo "::endgroup::"
+
+python3 /tools/analyze_results.py $output_dir/example_validation.xml $allow_level
